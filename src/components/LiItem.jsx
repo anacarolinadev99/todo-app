222f6c1706a0898850baa33d66e6e49e5a35cb96
Ana Carolina <anacarolinadev99@gmail.com>
2026-02-06 03:38:42 -0300

    Style: Adicionado responsividade para mobile e desktop

--- src/components/LiItem.jsx
+++ src/components/LiItem.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from './Button';
 import { Input } from './Input';
 
 export function LiItem({ todo, toggleTodo, deleteTodo }) {
+    //Monitora o estado do menu
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <li>
             <div className="task-container">
@@ -14,6 +17,20 @@ export function LiItem({ todo, toggleTodo, deleteTodo }) {
                 <p>{todo.task}</p>
             </div>
             <div className="buttons-li-container">
+                <Button
+                    classList={`menu-mobile-button ${isOpen ? 'open' : 'closed'}`}
+                    iconClass={isOpen ? 'bi bi-x-lg' : 'bi bi-list'}
+                    onClick={() => setIsOpen(!isOpen)}
+                />
+                <div
+                    className={`menu-mobile-container ${isOpen ? 'open' : 'closed'}`}
+                >
+                    <Button
+                        classList={`button-mobile mobile-delete ${isOpen ? 'open' : 'closed'}`}
+                        label="Deletar"
+                        onClick={() => deleteTodo(todo.id)}
+                    />
+                </div>
                 <Button
                     classList="button-li li-delete"
                     iconClass="bi bi-trash-fill"
